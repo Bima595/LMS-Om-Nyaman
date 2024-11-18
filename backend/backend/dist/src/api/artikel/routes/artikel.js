c77fdf5519cba1135d3169815dfb9a25fdea2171
Bima Bimo <91863257+Bima595@users.noreply.github.com>
2024-11-18 15:34:39 +0700

    Delete backend/backend directory

--- backend/backend/dist/src/api/artikel/routes/artikel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * artikel router
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::artikel.artikel');
